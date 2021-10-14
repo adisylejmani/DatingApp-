10bfaf60121c39476ea2a94c04c57f46d973cfdc
adisylejmani <adisylejmani123@gmail.com>
2021-10-14 16:13:12 +0200

    End of Section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,13 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component'; 
+import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module'; 
+import { ShareModule } from './_modules/share.module';
 
 
 @NgModule({
@@ -15,14 +21,19 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
