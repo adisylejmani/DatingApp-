3f1e8e245f6e024121691c87e4a5a8b211da5760
adisylejmani <adisylejmani123@gmail.com>
2021-10-18 16:30:16 +0200

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -15,6 +16,27 @@ namespace API.Entities
         public byte[] PasswordHash { get; set; }
 
         public byte[] PassworSalt { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }   = DateTime.Now;
+        public DateTime LastActive { get; set; } =DateTime.Now;
+        public string Gender { get; set; } 
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        // public int GetAge()
+        // {
+        //     return DateofBirth.CalculateAge();
+        // }
+
 
 
 
