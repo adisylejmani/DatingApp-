272366dc14ff496cbc9cc595dcc8f23fb22194d5
adisylejmani <adisylejmani123@gmail.com>
2021-10-26 13:43:57 +0200

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -32,11 +32,11 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateofBirth.CalculateAge();
-        // }
+        public ICollection<UserLike> LikedByUsers { get; set; }
 
+        public ICollection<UserLike> LikedUsers { get; set; }
+
+      
 
 
 
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -7,7 +7,6 @@ import { Member } from '../_models/member';
 import { PaginatedResult } from '../_models/pagination';
 import { User } from '../_models/user';
 import { UserParams } from '../_models/userparams';
-
 import { AccountService } from './account.service';
 
 
@@ -97,10 +96,21 @@ export class MembersService {
 
   setMainPhoto(photoId: number){
     return this.http.put(this.baseUrl+'users/set-main-photo/'+photoId, {});
-  }
+  } 
 
   deletePhoto(photoId: number){
     return this.http.delete(this.baseUrl+'users/delete-photo/'+ photoId);
+  }
+
+  addLike(username: string){
+    
+    return this.http.post(this.baseUrl+'likes/'+username, {});
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize){
+    let params= this.getPaginationHeaders(pageNumber,pageSize);
+    params=params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl+'likes',params);
   }
 
   private getPaginatedResult<T>(url, params) {
